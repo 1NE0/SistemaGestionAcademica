8b9047ce14837e4186197a73201d253c0c63a31a
juan <juan@localhost.localdomain>
2020-09-01 15:14:48 -0500

    se agrega historial de pagos

--- Academia_Arte_y_Vida/app/gestionacademica/views.py
+++ Academia_Arte_y_Vida/app/gestionacademica/views.py
@@ -9,7 +9,10 @@ from Academia_Arte_y_Vida.app.gestionacademica.forms import login_form
 from django.contrib.auth.decorators import login_required
 from Academia_Arte_y_Vida.app.gestionacademica.models import *
 from Academia_Arte_y_Vida.app.gestionacademica import models
-from Academia_Arte_y_Vida.app.gestionacademica.models import Estudiantes
+from Academia_Arte_y_Vida.app.gestionacademica.models import Estudiantes,Pagos,Detalle_Pagos
+from datetime import datetime
+from django.shortcuts import redirect
+
 # Create your views here.
 
 def Index(request):
@@ -84,3 +87,48 @@ def logout_user(request):
        user = request.user
        logout(request)
        return render(request , "index.html")
+
+
+
+#logica pagos#####################################################################################
+def buscarEstudiante(request):
+
+
+    return render(request,"buscarEstudiante.html")
+
+def buscar(request):
+    if request.GET["id"]:
+       idEstudiante = request.GET["id"] #asigno los datos de el campo a una variable
+       estudiantes=Estudiantes.objects.filter(identificacion__icontains=idEstudiante)
+       programas=models.Programas.objects.all()
+       buscarPago=models.Detalle_Pagos.objects.filter(Estudiante=idEstudiante)
+       fechaActual = datetime.now()
+       return render(request,"registrarpago.html",{"buscarP":buscarPago,"programaobj":programas,"Estudianteobj":estudiantes, "fechaActual":fechaActual, "query":idEstudiante})
+    else:
+        mensaje="no se ingresaron datos"
+
+    return HttpResponse(mensaje) #objeto http 
+
+def agregarpago(request):
+    motivo = request.GET["motivos"]
+    idestudiante = request.GET["idestudiante"]
+    estuidante= Estudiantes.objects.get(identificacion=idestudiante)
+    idpagos = request.GET["idpago"]
+    programalabel = request.GET["programas"]
+    programa1=models.Programas.objects.get(nom_programa=programalabel)
+    monto = request.GET["monto"]
+    fechahoy = datetime.now()
+    #prgramas = request.GET["programas"]
+    p = Pagos(id=idpagos, Programa=programa1,motivo=motivo).save()
+    p1=Pagos(Programa=programa1,id=idpagos,motivo=motivo)
+    Dp=Detalle_Pagos(Estudiante=estuidante,Pagos=p1,monto=monto,Fecha=fechahoy).save()
+    #return render(request,"pago#2.html",Dp="detallep",)   
+    return redirect('/')
+
+
+def historiaPagos(request):
+    idEstudiante = request.GET["id"] #asigno los datos de el campo a una variable        
+    estudiantes=Estudiantes.objects.filter(identificacion__icontains=idEstudiante)
+    buscarPago=models.Detalle_Pagos.objects.filter(Estudiante=idEstudiante)
+
+    return render(request,"historiaPagos.html",{"buscarP":buscarPago})
