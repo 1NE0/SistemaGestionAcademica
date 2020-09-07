01afae3257e738a974d8177f314526b9825118bd
NE0 <seas19754@gmail.com>
2020-09-07 17:34:02 -0500

    la pestaña de programa, cambia dependiendo el rol

--- Academia_Arte_y_Vida/app/gestionacademica/views.py
+++ Academia_Arte_y_Vida/app/gestionacademica/views.py
@@ -43,6 +43,9 @@ def Admision(request):
 def Programas(request):
     docente = None
     estudiante = None
+    #lists
+    temp = []
+    listaInscripciones = []
 
     programasLista = models.Programas.objects.all()
     userID = request.user.id
@@ -51,12 +54,14 @@ def Programas(request):
     
     estudiante = Estudiantes.objects.get(user_id=userID)
     inscripcionesEstudiante = Inscripciones.objects.filter(Estudiante_id = estudiante.identificacion)
-    print(inscripcionesEstudiante['id'])
+    #programas = [v['Programa_id'] for v in inscripcionesEstudiante.values()]
+    
+    
     
     
     
 
-    return render(request, "programas.html", {'programasLista': programasLista , 'estudiante' : estudiante , 'docente' : docente , 'inscripciones' : inscripcionesEstudiante.values()})
+    return render(request, "programas.html", {'programasLista': programasLista , 'estudiante' : estudiante , 'docente' : docente , 'inscripcionesEstudiante' : inscripcionesEstudiante})
 
 
 login_required(login_url='/login/login.html')
