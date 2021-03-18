88a07526b9ba5710e51aafc6928369b3cc233c3d
howl <seas19754@gmail.com>
2021-03-17 21:26:14 -0500

    organizando

--- static/js/board_Docente/panel.js
+++ static/js/board_Docente/panel.js
@@ -3,21 +3,21 @@ $(document).ready(function(){
 
     $('#btn1').on('click', function(c){
         c.preventDefault();
+        $("#contenedor-central").load('/cargando');
         $("#contenedor-central").load('/cursosDocente');
-        $("#contenedor-central").html('<h1 style="text-align:center;padding-top:300px;color:cyan">Cargando<h1>');
 
     });
 
     $('#btn2').on('click', function(asi){
         asi.preventDefault();
+        $("#contenedor-central").load('/cargando');
         $("#contenedor-central").load('/asignaturasDocente');
-        $("#contenedor-central").html('<h1 style="text-align:center;padding-top:300px;color:cyan">Cargando<h1>');
     });
 
     $('#btn3').on('click', function(actividades){
         actividades.preventDefault();
+        $("#contenedor-central").load('/cargando');
         $("#contenedor-central").load('/actividadesDocente');
-        $("#contenedor-central").html('<h1 style="text-align:center;padding-top:300px;color:cyan">Cargando<h1>');
     });
 
 });
