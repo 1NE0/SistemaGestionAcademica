fc7304703ae58c78f5009f212bbd6a4ee7ad5358
howl <seas19754@gmail.com>
2021-03-25 12:16:53 -0500

    se agregan archivos staticos

--- Academia_Arte_y_Vida/settings.py
+++ Academia_Arte_y_Vida/settings.py
@@ -150,7 +150,7 @@ DATE_INPUT_FORMATS = (
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-
+STATIC_URL = '/static/'
 
 """ STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),) 
 # opciones posibles: 'sweetalert', 'sweetalert2' - el valor predeterminado es 'sweetalert2' 
