0df594b5c5f8b87fb0a065c931a5379eb96c8333
Juan Camilo Castillo <juan.castillo.jimenez@correounivalle.edu.co>
2021-04-08 20:01:49 -0500

    validaciones editar estudiante y docente

--- static/js/validacion_docentes.js
+++ static/js/validacion_docentes.js
@@ -3,6 +3,7 @@ const formulario = document.getElementById('formCrearDocente');
 //obtener todos los inputs del fromulario
 const inputs = document.querySelectorAll('#formCrearDocente input');
 
+
 const expresiones = {
 	usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
 	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
